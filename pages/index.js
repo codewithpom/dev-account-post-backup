b48758800790c03777eaa7a9faeaa896baba3dd7
Padmashree Jha <79435304+codewithpom@users.noreply.github.com>
2022-01-21 16:32:58 +0530

    Seems like done

--- pages/index.js
+++ pages/index.js
@@ -1,205 +1,276 @@
-import Head from 'next/head'
+import Head from "next/head"
+import { useState } from "react"
+import { Octokit } from "@octokit/rest"
+import ProgressBar from "@ramonak/react-progress-bar";
+
 
 export default function Home() {
+  const [createNewRepository, setCreateNewRepository] = useState(true);
+  const [fetching, setFetching] = useState(false);
+  const [progress, setProgress] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [display, setDisplay] = useState("none");
+  const [username, setUsername] = useState("");
+  const [repoName, setRepoName] = useState("");
+  const [githubToken, setGithubToken] = useState("");
+  const [isPrivate, setIsPrivate] = useState(false);
+  async function getPosts(username, pageNumber) {
+    if (pageNumber === undefined) {
+      pageNumber = 1
+    }
+    // fetch blogs for user from dev.to
+    const res = await fetch(`https://dev.to/api/articles?username=${username}&page=${pageNumber}`)
+    const data = await res.json()
+    return data;
+  }
+  async function sleep(seconds) {
+    return new Promise(resolve => setTimeout(resolve, seconds * 1000));
+  }
+  async function fetchAllPosts() {
+    let pageNumber = 1;
+    let data = await getPosts(username, pageNumber);
+    let posts = data;
+    if (posts.length !== 30) {
+      return posts;
+    }
+
+    while (true) {
+      pageNumber++;
+      data = await getPosts(username, pageNumber);
+      posts = posts.concat(data);
+      if (data.length !== 30) {
+        return posts;
+      }
+    }
+
+
+  }
+
+  async function fetchReadme(blog_id) {
+    while (true) {
+      try {
+        // fetch the details about the blog from dev.to and then console log the markdown
+        const res = await fetch(`https://dev.to/api/articles/${blog_id}`)
+        const data = await res.json()
+
+        return data.body_markdown
+
+      }
+      catch (error) {
+        await sleep(3);
+      }
+    }
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+    const octokit = new Octokit({
+      auth: githubToken
+    });
+
+
+
+    setFetching(true);
+    const posts = await fetchAllPosts()
+    setFetching(false);
+    setTotal(posts.length)
+    setProgress(0);
+    setDisplay("");
+    if (createNewRepository) {
+      const { data } = await octokit.repos.createForAuthenticatedUser({
+        name: repoName,
+        description: "This is a test repository",
+        private: isPrivate
+      });
+    }
+
+    let progress_var = 0;
+    const owner = (await octokit.users.getAuthenticated()).data.login
+    for (let post of posts) {
+      const readme = await fetchReadme(post.id)
+      const { data } = octokit.rest.repos.createOrUpdateFileContents({
+        owner: owner,
+        repo: repoName,
+        path: `${post.id}.md`,
+        message: "Test commit",
+        content: Buffer.from(readme).toString("base64"),
+        committer: {
+          email: "test@test.org",
+          name: "DEV.to backup Bot",
+        }
+
+      }).then(res => {
+        setProgress(progress_var + 1)
+        progress_var++;
+      })
+      await sleep(1)
+    }
+  }
   return (
     <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <br />
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js</a> on{' '}
-          <a href="https://gitpod.io/" style={{ color: '#FFB45B' }}>
-            Gitpod!
-          </a>
+      <div className="text-center">
+        <h1>
+          Backup all your dev.to posts
         </h1>
+      </div>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a href="https://github.com/vercel/next.js/tree/master/examples" className="card">
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
+      <br />
+      {/*Create a bootstrap form */}
+      <form onSubmit={handleSubmit}>
+        <div className={"form-group"}>
+          <label
+            htmlFor={"username"}
           >
-            <h3>Deploy &rarr;</h3>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
-      </main>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
-      </footer>
+            DEV Username
+          </label>
+          <input
+            type={"text"}
+            className={"form-control"}
+            id={"username"}
+            aria-describedby={"DEVUsername"}
+            placeholder={"Enter username"}
+            value={username}
+            onChange={
+              (e) => {
+                setUsername(e.target.value)
+              }
+            }
+            required
+          />
+          <br />
+          <label htmlFor="repoName">
+            Repo Name
+          </label>
+          <input
+            type={"text"}
+            className={"form-control"}
+            id={"repoName"}
+            aria-describedby={"RepoName"}
+            placeholder={"Enter Repo Name"}
+            value={repoName}
+            onChange={
+              (e) => {
+                setRepoName(e.target.value)
+              }
+            }
+            required
+          />
 
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
+          <br />
+          <label htmlFor="githubToken">
+            Github Token
+          </label>
+          <input
+            type={"text"}
+            className={"form-control"}
+            id={"githubToken"}
+            aria-describedby={"GithubToken"}
+            placeholder={"Enter Github Token"}
+            value={githubToken}
+            onChange={
+              (e) => {
+                setGithubToken(e.target.value)
+              }
+            }
+            required
+          />
+          <br />
+          <div className="form-group form-check">
+            <input
+              type={"checkbox"}
+              className={"form-check-input"}
+              id={"isPrivate"}
+              aria-describedby={"IsPrivate"}
+              placeholder={"Is Private"}
+              checked={isPrivate}
+              onChange={
+                (e) => {
+                  setIsPrivate(e.target.checked)
+                }
+              }
 
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
+            />
+            <label htmlFor="isPrivate" className="form-check-label">
+              Is Private
+            </label>
 
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
+          </div>
 
-        footer img {
-          margin-left: 0.5rem;
-        }
+          <div className="form-group form-check">
+            <input
+              type={"checkbox"}
+              className={"form-check-input"}
+              id={"initializeRepo"}
+              aria-describedby={"initializeRepo"}
+              placeholder={"Initialize Repo"}
+              checked={createNewRepository}
+              onChange={
+                (e) => {
+                  setCreateNewRepository(e.target.checked)
+                }
+              }
 
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
+            />
+            <label htmlFor="initializeRepo" className="form-check-label">
+              Initialize new repository
+            </label>
 
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
+          </div>
 
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono, Bitstream Vera Sans Mono,
-            Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
+          {
+            fetching ?
+              <div className="text-center">
+                <div className="spinner-border text-primary" role="status">
+                  <span className="sr-only">Fecthing posts...</span>
+                </div>
+              </div>
+              :
+              <></>
           }
-        }
-      `}</style>
 
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans,
-            Droid Sans, Helvetica Neue, sans-serif;
-        }
+        </div>
 
-        * {
-          box-sizing: border-box;
+        <button
+          type={"submit"}
+          className={"btn btn-primary"}
+        >
+          Submit
+        </button>
+      </form>
+      <br />
+      <div style={{ display: display }}>
+        <h3 className="text-center">
+          There are total {total} posts to backup
+        </h3>
+        <br />
+        <ProgressBar
+          completed={`${progress}`}
+          maxCompleted={`${total}`}
+          bgColor="blue"
+        />
+        {
+          progress === total ?
+            <>
+              <br />
+              <h3 className="text-center">
+                Backup Completed
+              </h3>
+            </>
+            :
+            <>
+              <br />
+              <h3 className="text-center">
+                Backup in progress
+              </h3>
+            </>
         }
-      `}</style>
+      </div>
+
     </div>
+
   )
 }
